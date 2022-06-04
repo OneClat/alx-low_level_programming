7180375d7d81e1a04063a5ac6008944bf69d82cf
OneClat <dammecares@gmail.com>
2022-06-04 11:30:25 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,17 +9,10 @@ int main(void)
 {
 	int ch;
 	for (ch = 97; ch <= 122; ch++)
-<<<<<<< HEAD
 
 	{
 		putchar(ch);
 	}
-=======
-	{
-		putchar(ch);
-	}
-	putchar(ch = 10); /** this is an ascii code for new line **/
->>>>>>> edbb6b3dc98986d74631b576203d881e1509a12b
 	return (0);
 }
 
