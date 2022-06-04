c8674c0a67f9456e1793886d9180b21283d33511
OneClat <dammecares@gmail.com>
2022-06-04 10:01:30 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * *main -> assign a random number to the variable n each time it is executed
+ * *based a condition
+ * *Return: Always 0 (SUCCESS)
+ * **/
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /** this is an ascii code for new line **/
+	return (0);
+}
