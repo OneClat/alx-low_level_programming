b3f143a5aa3fcb96f83b47f39a3240e08b33d7eb
OneClat <dammecares@gmail.com>
2022-06-15 16:23:17 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,9 +2,9 @@
 
 /**
  * * main - Entry point
- * * 
+ * *
  * * Description: 'the program's description'
- * * 
+ * *
  * * Return: Always 0 (Success)
  * **/
 
@@ -28,4 +28,4 @@ int main(void)
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-}
+
